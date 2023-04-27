30461c5c764eede50cf0752c4c202f29ea9aab5d
vini3821 <63987681+vini3821@users.noreply.github.com>
2023-04-27 20:39:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
 
-float x = 0; 
-float y = 0;
-float res= 0; 
-
+int x = 0;
+int y = 0;
+char sinal = ' ';
+float res = 0;
 int main()
 {
+    
+    printf("Digite a operação desejada em sinais matematicos: ");
+    scanf("%c", &sinal );
     printf("Digite um número: ");
-    scanf("%f", &x);
-    
+    scanf("%i", &x);
     printf("Digite outro número: ");
-    scanf("%f", &y);
-    
-    //soma
-    
-    res = x + y;
-    printf("A soma de %.2f e %.2f = %.2f \r\n", x, y, res);
-    
-    //subtração
-    res = x - y;
-    printf("A subtração de %.2f e %.2f = %.2f \r\n", x, y, res);
-    
-    //multiplicação
-    
-    res = x * y;
-    printf("A multiplicação de %.2f e %.2f = %.2f \r\n", x, y, res);
-    
-    //Divisão
-    
-    res = x / y;
-    if(y == 0){
-        printf("O número divisor é 0, logo o resultado será 0");
-    }else{
-        printf("A Divisão de %.2f e %.2f = %.2f \r\n", x, y, res);
+    scanf("%i", &y);
+    switch(sinal){
+        case '+':
+            res = x + y;
+            printf("A soma de %.2i e %.2i = %.2f \r\n", x, y, res);
+        
+        break;
+        case '-':
+            res = x - y;
+            printf("A subtração de %.2i e %.2i = %.2f \r\n", x, y, res);
+        
+        break;
+        case '*':
+            res = x * y;
+            printf("A multiplicação de %.2i e %.2i = %.2f \r\n", x, y, res);
+             
+        break;    
+        case '/': 
+        
+        
+        if (y == 0){
+            printf("Não pode ser dividido");
+        }
+        else{
+            res = x / y;
+            printf("A Divisão de %.2i e %.2i = %.2f \r\n", x, y, res);
+        }
+        break;
     }
-
     return 0;
+    
 }
